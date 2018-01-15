2a800daa0eaba05019faa7b78cc280002860c6cb
klinga <klingaroo@gmail.com>
2018-01-15 15:38:51 -0500

    expanded db tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -13,23 +13,74 @@ class TestDatastore(unittest.TestCase):
     def setUpClass(cls):
         db.dal.conn_string = 'sqlite:///:memory:'
         db.dal.connect()
-        db.dal.session = db.dal.Session()
-        enter_test_data(db.dal.session)
-        db.dal.session.close()
+        init_session = db.dal.Session()
+        enter_test_data(init_session)
+        init_session.close()
 
     def setUp(self):
-        db.dal.session = db.dal.Session()
+        # if not using session manager (context_session)
+        # don't forget to commit each action
+        self.session = db.dal.Session()
 
     def tearDown(self):
-        db.dal.session.rollback()
-        db.dal.session.close()
+        self.session.rollback()
+        self.session.close()
+
+    def test_run_query(self):
+        # print self.session
+        resA = worker.run_query(self.session, "SELECT * FROM conflicts").first()
+        resB = self.session.execute('SELECT * FROM conflicts').first()
+        self.session.commit()
+        self.assertEqual(
+            resA.code, resB.code)
 
     def test_delete_table_data(self):
-        self.assertEqual(worker.delete_table_data(db.Bibs), 6)
-        self.assertEqual(worker.delete_table_data(db.Orders), 6)
+        res = worker.delete_table_data(
+            self.session, db.Bibs)
+        self.assertEqual(res, 6)
+        res = self.session.execute('SELECT * FROM bibs').fetchall()
+        self.session.commit()
+        self.assertEqual(len(res), 0)
+
+    def test_insert_or_ignore_ignore_scenario(self):
+        worker.insert_or_ignore(
+            self.session,
+            db.Conflicts, id=1, desc='test 1 desc')
+        res = self.session.execute('SELECT * FROM conflicts WHERE id = 1').first()
+        self.session.commit()
+        self.assertEqual(res.desc, 'Error Test 1')
+
+    def test_insert_or_ignore_insert_scenario(self):
+        worker.insert_or_ignore(
+            self.session,
+            db.Conflicts, id = 4, level='bib-ord', code='ErrB002', desc = 'TEST insert')
+        self.session.commit()
+        self.session.rollback()
+
+        res = self.session.execute('SELECT * FROM conflicts WHERE id = 4').first()
+        self.session.commit()
+        self.assertEqual(res.id, 4)
 
 
+    def test_insert_or_update_insert_scenario(self):
+        res = self.session.execute('SELECT * FROM conflicts WHERE id = 5').first()
+        # self.session.commit()
+        self.assertIsNone(res)
+        worker.insert_or_update(
+            self.session,
+            db.Conflicts,
+            id = 5, level='bib-ord', code='ErrB003', desc = 'TEST insert on insert_or_update')
+        self.session.commit()
+        res = self.session.execute('SELECT * FROM conflicts WHERE id = 5').first()
+        self.assertEqual(res.id, 5)
 
+    def test_insert_or_update_update_scenario(self):
+        worker.insert_or_update(
+            self.session,
+            db.Conflicts, id = 5, level='bib')
+        self.session.commit()
+        res = self.session.execute('SELECT * FROM conflicts WHERE id = 5').first()
+        self.assertEqual(res.level, 'bib')
 
 
 if __name__ == '__main__':
