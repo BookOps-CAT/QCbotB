9b013c3ba1db63e74251223220d8b6b4c0186f63
Tomasz Kalata <klingaroo@gmail.com>
2018-01-26 09:50:17 -0500

    logging added

--- QCbotB/ftp_worker.py
+++ QCbotB/ftp_worker.py
@@ -1,14 +1,17 @@
 # responsible for fetching and managing ftp files
+from ftplib import FTP, error_reply
+from datetime import datetime
+import logging
+
 
 # TODO:
 # - move downloaded file to archive
 # - delete reports older than 30 days from archive
 
-from ftplib import FTP, error_reply
-from datetime import datetime
+module_logger = logging.getLogger('QCBtests')
 
 
-def todays_file(dir_list=None):
+def find_todays_file(dir_list=None):
     """
     identifies filename of most recent Sierra report
     """
@@ -22,29 +25,44 @@ def todays_file(dir_list=None):
 
 
 def ftp_download(host, user, passw, folder):
+    fetched = False
     try:
+        module_logger.info('ftp_worker: Connecting to FTP...')
         ftp = FTP(host)     # connect to host, default port
         res = ftp.login(user, passw)
         if res[:3] == '230':  # '230 Login successful.'
+            module_logger.info('fpt_worker: Succesfully connected to FTP...')
             ftp.cwd(folder)  # change to desired directory
-            files = []
-            # ftp.retrlines('NLST', files.append)
-            files = ftp.nlst()
-            report = todays_file(files)
-            ftp.retrbinary(
-                'RETR {}'.format(report),
-                open('./files/report.txt', 'wb').write)
+            files = ftp.nlst()  # retrieve all files in the folder
+            todays_file = find_todays_file(files)
+            if todays_file is None:
+                module_logger.warning(
+                    'ftp_worker: not able to find sierra report')
+            else:
+                module_logger.info(
+                    'ftp_worker: Identified todays sierra report')
+                ftp.retrbinary(
+                    'RETR {}'.format(todays_file),
+                    open('./files/report.txt', 'wb').write)
+                fetched = True
+                module_logger.info(
+                    'fpt_worker: Downloaded todays sierra report')
         else:
-            # log?
-            print 'ftp included respons other than 230: {}'.format(res)
+            module_logger.error(
+                'ftp_worker: Unsucessful login to FTP server, '
+                'FTP response: {}'.format(res))
     except error_reply:
-        # log error
-        print error_reply
+        module_logger.error(
+            'ftp_worker: Encountered following error: {}'.format(
+                error_reply))
     finally:
         try:
             ftp.close()
+            module_logger.info(
+                'ftp_worker: FTP connection closed succesfully.')
         except error_reply:
             ftp.quit()
+        return fetched
 
 
 if __name__ == '__main__':
