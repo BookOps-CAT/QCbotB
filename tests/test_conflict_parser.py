4080668144629b9335b7ffe465dd3cf6b54fe8fb
Tomasz Kalata <klingaroo@gmail.com>
2018-05-17 09:28:13 -0400

    updated to new conflicts.xml format

--- tests/test_conflict_parser.py
+++ tests/test_conflict_parser.py
@@ -19,14 +19,14 @@ class TestConflictParser(unittest.TestCase):
     def test_conflict2dict_output_unit_dict_keys(self):
         res = conflict2dict('conflicts_test.xml')
         keys = [k for u in res for k in u]
-        self.assertEqual(keys, ['query', 'level', 'code', 'id', 'desc'])
+        self.assertEqual(keys, ['tier', 'query', 'code', 'description', 'id'])
 
     def test_conflict2dict_output_values(self):
         res = conflict2dict('conflicts_test.xml')
         c = res[0]
         self.assertEqual(c['id'], 1)
         self.assertEqual(c['code'], 'ErrA001')
-        self.assertEqual(c['level'], 'bib')
+        self.assertEqual(c['tier'], 'bib-ord')
         self.assertEqual(c['query'], 'SELECT * FROM bibs WHERE bibs.c_cutter = 0 AND bibs.c_type != "fea" AND (bibs.c_type != "eas" AND bibs.author IS NULL)')
 
 
