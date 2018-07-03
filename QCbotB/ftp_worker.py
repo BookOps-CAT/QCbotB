4cabea83bc80fbf785a4f20751a8184193beb72a
Tomasz Kalata <klingaroo@gmail.com>
2018-07-03 11:26:41 -0400

    sublogger specified

--- QCbotB/ftp_worker.py
+++ QCbotB/ftp_worker.py
@@ -6,7 +6,7 @@ import logging
 
 # ToDo: tests for find_todays_file and aged_out_report
 
-module_logger = logging.getLogger('QCBtests')
+module_logger = logging.getLogger('QCBtests.ftp_worker')
 
 
 def find_todays_file(dir_list=None):
@@ -59,7 +59,7 @@ def ftp_maintenance(host, user, passw, folder):
             files = ftp.nlst()  # retrieve all files in the folder
             todays_file = find_todays_file(files)
             if todays_file is not None:
-                files.remove(todays_file)  # skip todays file in the maintenence
+                files.remove(todays_file)
             try:
                 module_logger.info(
                     'ftp_worker: Begining moving old files to archive...')
